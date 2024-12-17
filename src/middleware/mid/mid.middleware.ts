7b280973dd5d95f643d11bda6f138296b4a5fed5
vh4 <fathoniwasesojati@gmail.com>
2024-12-17 16:22:55 +0700

    update

--- src/middleware/mid/mid.middleware.ts
+++ src/middleware/mid/mid.middleware.ts
@@ -6,6 +6,6 @@ export class MidMiddleware implements NestMiddleware {
   constructor(private readonly mid: HelpersService) {}
   use(req: any, res: any, next: () => void) {
     req.mid = this.mid.Mid();
-    next();
+    next(); //
   }
 }
